56423e4cc2c173a875b1ba054e07623940cb5dad
PapaKarlo787 <33232815+PapaKarlo787@users.noreply.github.com>
2022-05-06 14:08:57 +0300

    Update mcasm.py

--- mcasm.py
+++ mcasm.py
@@ -23,7 +23,7 @@ commands = {"add": add, "sub": sub, "mul": mul, "div": div, "mov": mov,
 			"pushaf": pushaf, "rpix": rpix, "nop": nop, "mzer": mzer,
 			"test": test, "ftest": ftest, "gtm": get_time, "stm": set_time,
 			"mls": millis, "mcs": micros, "gmice": gmice, "gjoy": gjoy,
-			"gir": gir}
+			"gir": gir, "btest": btest}
 
 
 pattern = re.compile(r"\".*\"|\[|\]|\+|\*|-?[\w\.]+|,|:|;.*|-|\$")
